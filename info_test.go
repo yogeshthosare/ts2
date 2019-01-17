6e45cafaf0f8bf579cb597fc076dd584d0eb9f3e
yogesh <yogesh@aotpl.com>
2019-01-17 18:20:35 +0530

    update test script

--- info_test.go
+++ info_test.go
@@ -6,9 +6,6 @@ import (
     "log"
     "os/user"
     "strconv"
-    "regexp"
-    "fmt"
-    "io/ioutil"
 )
 
 func TestInfo(t *testing.T){
@@ -23,23 +20,6 @@ func TestInfo(t *testing.T){
 	var expectedUid = user.Uid
 	var expectedPid = strconv.Itoa(os.Getpid())
 
-	filename := "/proc/"+uid+"/status"
-	file, err := ioutil.ReadFile(filename)
-        if err != nil {
-            fmt.Print(err)
-        }
-
-        filecontent := string(file)
-
-	re := regexp.MustCompile(`1000`)
-	matches := re.FindStringSubmatch(filecontent)
-	fmt.Println(matches[1])
-
-	
-	re = regexp.MustCompile(`17183`)
-	matches = re.FindStringSubmatch(filecontent)
-	fmt.Println(matches[1])
-
 	if uid != expectedUid {
                 t.Fatalf("Expected %s but got %s", uid, expectedUid)
         }
@@ -47,4 +27,3 @@ func TestInfo(t *testing.T){
                 t.Fatalf("Expected %s but got %s", pid, expectedPid)
         }
 }
-
